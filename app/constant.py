bf492fa314f972eac9ba92cd0a53970d86643c0a
Bruno Barroso <brunobinfo@gmail.com>
2019-01-14 19:37:14 +0000

    Add more constants

--- app/constant.py
+++ app/constant.py
@@ -1,7 +1,7 @@
-# constant.py
-
-ROOT_PATH               = "C:/Users/Integer/Documents/python/"
+ROOT_PATH               = "C:/Users/Integer/Documents/iostreams/"
 PATH_INPUT              = ROOT_PATH + "storage/input/"
 PATH_OUTPUT             = ROOT_PATH + "storage/output/"
 STRING_BLOCK_SEPARATOR  = "firstName|lastName|birthday|"
 EXT_DEFAULT_OUTPUT      = ".txt"
+STRING_LETTERS          = 'asdfghjklpoiuytrewqzxcvbnm'
+EXT_ALLOWED_INPUT       = ["*.xml", "*.txt", "*.ubl", "*.pdf", "*.jpg"]
