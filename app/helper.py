ddf27ad38b7f29e343975427ca0528fea56288bc
Bruno Barroso <brunobinfo@gmail.com>
2019-01-14 19:39:51 +0000

    Create some functions responsable split arrays, logger and generate identify

--- app/helper.py
+++ app/helper.py
@@ -1,7 +1,37 @@
-# helper.py
+import random
+import time
+from app import constant as c
 
-def EdpLogger( message, modo='info' ):
+def logger( message, modo='info' ):
     print(f"Debug [{modo}]: {message}")
 
-def QueryInsertInvoiceTemplate():
+def queryInsertInvoiceTemplate():
     return """ INSERT INTO `invoice` (`id`, `identify`, `status`) VALUES (null,%s, %s)"""
+
+def queryInsertInvoiceItemTemplate():
+    return """ INSERT INTO `invoice_items` (`id`, `invoice_id`, `content`) VALUES (null,%s, %s)"""
+
+def generateIdentify():
+    logger(f"Generate identify") # logging activities
+    return str( random.choice(c.STRING_LETTERS) + random.choice(c.STRING_LETTERS) + str(time.time()).replace('.', '') ).upper()
+
+def splitList( lists, qty ):
+    groups = {}
+    indexLists = 0
+    qtyItemsByList = int(len(lists)/qty) # number of invoices per group
+    for group in range(0, qty):
+        items = list()
+        for item in range(0, qtyItemsByList):
+            if item == 0 and indexLists > 0: # adds the last position of the previous block
+                items.append( lists[indexLists-1] )
+            items.append( lists[indexLists] ) # adds the positions of the current block
+            if item == (qtyItemsByList-1) and indexLists > 0 and group < (qty-1): # adds the first position of the next block
+                items.append( lists[indexLists+1] )
+            logger(f"Group of intervals {group+1}/{qty} - item {item+1}/{qtyItemsByList} - {lists[indexLists]} - {indexLists}") # logging activities
+            indexLists += 1
+        groups[group] = items
+    return groups
+
+def replaceFilename( filename ):
+      array = filename.split('\\')
+      return array[len(array)-1]
