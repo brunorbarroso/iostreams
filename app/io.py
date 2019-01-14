fc43da100a5c7033f74105868ad2d717df662c7d
Bruno Barroso <brunobinfo@gmail.com>
2019-01-14 19:38:37 +0000

    Refactoring class and remove some libraries

--- app/io.py
+++ app/io.py
@@ -1,13 +1,11 @@
-# app.py
-
 import sys
-import datetime as dt
+import time
 from itertools import cycle
 
 # required project
 from app import constant as c
 from app import helper as h
-from app import db
+from app import database as db
 from app import helper
 
 ## 
@@ -16,12 +14,10 @@ from app import helper
 class IOStreams:
 
     def __init__( self ):
-        self.database = db.Database( databaseUser="root", 
-                            databasePassword="", 
-                            databaseHost="127.0.0.1", 
-                            databaseName="parse")
-        self.database.connector()
-        self.database.execute( helper.QueryInsertInvoiceTemplate(), ("UHAHUHUA1", "waiting"))
+        self.database = db.Database(    databaseUser="root", 
+                                        databasePassword="", 
+                                        databaseHost="127.0.0.1", 
+                                        databaseName="parse")
     
     def getBlockIntervalPositions( self, fileName ):
         positions = list()
@@ -29,21 +25,22 @@ class IOStreams:
         with open(fileName) as f:
             for num, line in enumerate(f, 1):
                 if c.STRING_BLOCK_SEPARATOR in line:
-                    num = str(num)
-                    positions.append(num)
+                    positions.append(str(num))
+            positions.append(str(num))
         return positions
     
     def createFile( self, content, rootPath=c.PATH_OUTPUT, ext=c.EXT_DEFAULT_OUTPUT ):    
-        pathAndFileName=rootPath+dt.datetime.now().strftime("%Y%m%d%H%M%S")+ext
+        pathAndFileName= rootPath + h.generateIdentify() + ext
         with open(pathAndFileName, 'w+') as f:
             for item in content:
                 f.write("%s\n" % item)
         f.close
+        h.logger(f"Created file")
     
-    def register( self, content, rootPath=c.PATH_OUTPUT, ext=c.EXT_DEFAULT_OUTPUT ):    
-        invoiceIdentify=rootPath+dt.datetime.now().strftime("%Y%m%d%H%M%S")
+    def register( self, content ):
+        invoiceId = self.database.execute( h.queryInsertInvoiceTemplate(), (h.generateIdentify().upper(), "waiting"))
         for item in content:
-            item
+            self.database.execute( h.queryInsertInvoiceItemTemplate(), (invoiceId, item.replace('\n','')))
 
     def extractContentBlockFileByInterval( self, fileName, start, end ):
         contentBlock=list()
@@ -68,8 +65,8 @@ class IOStreams:
             while running:
                 thisItem, nextItem = nextItem, next(listCycle)
                 block = self.extractContentBlockFileByInterval( fileName, start=thisItem, end=nextItem )
-                self.createFile( content=block )
+                self.register( content=block )
                 blocks.append( block )
-                h.EdpLogger(f"Capture Invoice Block #{str(item)}/{maxLine}: StartLine: {str(thisItem)} - EndLine: {str(nextItem)}")
+                h.logger(f"Capture Invoice Block #{str(item)}/{maxLine}: StartLine: {str(thisItem)} - EndLine: {str(nextItem)}")
                 break
         return blocks
