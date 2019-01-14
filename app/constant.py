fd8e66847ebf2b48de2e9a34602b65dea670b0d2
Bruno Barroso <brunobinfo@gmail.com>
2019-01-14 20:53:57 +0000

    Remove constants are not constants :p

--- app/constant.py
+++ app/constant.py
@@ -1,7 +1,8 @@
+import os
+
 ROOT_PATH               = "C:/Users/Integer/Documents/iostreams/"
 PATH_INPUT              = ROOT_PATH + "storage/input/"
 PATH_OUTPUT             = ROOT_PATH + "storage/output/"
-STRING_BLOCK_SEPARATOR  = "firstName|lastName|birthday|"
-EXT_DEFAULT_OUTPUT      = ".txt"
 STRING_LETTERS          = 'asdfghjklpoiuytrewqzxcvbnm'
 EXT_ALLOWED_INPUT       = ["*.xml", "*.txt", "*.ubl", "*.pdf", "*.jpg"]
+DELIMITER_PATH          = os.sep
