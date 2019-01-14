8d3d63b5cad33f79801a5e8721e12fae24c88e88
Bruno Barroso <brunobinfo@gmail.com>
2019-01-14 20:51:12 +0000

    Refactoring main file

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from app import io
 from app import helper as h
+import datetime as dt
 
 def run( pathAndFileName ):
+    startTime = dt.datetime.now().strftime("%Y/%m/%d at %H:%M:%S")
     h.logger(f"Initiating file data extraction process.")
     inputFileName = h.replaceFilename(pathAndFileName)
+    
     ios = io.IOStreams()
     positions = ios.getBlockIntervalPositions( inputFileName )
-    packages = h.splitList( positions, 450 )
-    for package in packages.items():
-      ios.getContentFileByInterval( inputFileName, package[1], len(package[1]) )
+    packages = h.splitList( positions, 2 )
+    if len(packages) > 0:
+      for num, package in enumerate(packages.items()):
+        ios.getContentFileByInterval( inputFileName, positions=package[1], maxLine=len(package[1]), batch=num+1 )
+    endTime = dt.datetime.now().strftime("%Y/%m/%d at %H:%M:%S")
     h.logger(f"Extraction process closed.")
+    h.logger(f"Started at {startTime} and finished at {endTime}")
+
+#run('.\\storage\\input\\text-small.txt') # call test
